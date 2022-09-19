3e598bc9d8aee12596d566320501221117cf142a
skipancho <ctg1770a@gmail.com>
2022-09-19 20:31:13 +0900

    [FEATURE] #9 - AuthRepository 구현 완료

--- app/src/main/java/com/jjsh/smartshopping/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/jjsh/smartshopping/domain/repository/AuthRepository.kt
@@ -2,4 +2,15 @@ package com.jjsh.smartshopping.domain.repository
 
 interface AuthRepository {
     suspend fun signin(userId: String, password: String): Result<Unit>
+
+    suspend fun signup(
+        userId: String,
+        password: String,
+        nickName: String,
+        name: String
+    ): Result<Unit>
+
+    suspend fun validateUserId(userId: String): Result<Unit>
+
+    suspend fun validateNickName(nickName: String): Result<Unit>
 }
