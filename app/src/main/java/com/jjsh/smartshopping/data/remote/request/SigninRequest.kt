55dd3880b2269a481a514d485916193bb20831f0
skipancho <ctg1770a@gmail.com>
2022-09-19 20:31:41 +0900

    [FEATURE] #9 - 불필요한 nullable 타입 수정

--- app/src/main/java/com/jjsh/smartshopping/data/remote/request/SigninRequest.kt
+++ app/src/main/java/com/jjsh/smartshopping/data/remote/request/SigninRequest.kt
@@ -1,10 +1,7 @@
 package com.jjsh.smartshopping.data.remote.request
 
 data class SigninRequest(
-    val userId: String?,
-    val password: String?
-) {
-    fun isNotValidUserId() = userId.isNullOrBlank()
-    fun isNotValidPassword() = password.isNullOrBlank() || password.length !in 2..20
-}
+    val userId: String,
+    val password: String
+)
 
--- app/src/main/java/com/jjsh/smartshopping/data/remote/request/SignupRequest.kt
+++ app/src/main/java/com/jjsh/smartshopping/data/remote/request/SignupRequest.kt
@@ -1,16 +1,8 @@
 package com.jjsh.smartshopping.data.remote.request
 
 data class SignupRequest(
-    val userId: String?,
-    val password: String?,
-    val nickName: String?,
-    val name: String?
-) {
-    fun isNotValidID() = userId.isNullOrBlank()
-
-    fun isNotValidPassword() = password.isNullOrBlank() || password.length !in 2..20
-
-    fun isNotValidNickName() = nickName.isNullOrBlank() || nickName.length !in 2..20
-
-    fun isNotValidName() = name.isNullOrBlank()
-}
+    val userId: String,
+    val password: String,
+    val nickName: String,
+    val name: String
+)
