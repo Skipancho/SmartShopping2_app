3379089aabfc1ae1cbf0aa13825d9880822b1076
skipancho <ctg1770a@gmail.com>
2022-09-28 20:28:39 +0900

    [FEATURE] #12 - 로그인 화면 UI 구현(임시)

---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/signin/SigninActivity.kt
@@ -0,0 +1,12 @@
+package com.jjsh.smartshopping.presentation.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jjsh.smartshopping.R
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
