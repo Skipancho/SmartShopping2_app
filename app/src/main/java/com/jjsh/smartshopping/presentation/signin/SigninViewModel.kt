31f3bbed8cd9993acf3139c945a949de8bc3b8f6
skipancho <ctg1770a@gmail.com>
2022-09-30 19:28:33 +0900

    [FEATURE] #12 - UiState에 따른 이벤트 처리

--- app/src/main/java/com/jjsh/smartshopping/presentation/signin/SigninViewModel.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/signin/SigninViewModel.kt
@@ -3,8 +3,10 @@ package com.jjsh.smartshopping.presentation.signin
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jjsh.smartshopping.domain.usecase.SigninUseCase
+import com.jjsh.smartshopping.presentation.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,17 +14,28 @@ import javax.inject.Inject
 class SigninViewModel @Inject constructor(
     private val signinUseCase: SigninUseCase
 ) : ViewModel() {
+    private val _uiState = MutableStateFlow<UiState<Unit>>(UiState.Init)
+    val uiState: StateFlow<UiState<Unit>> get() = _uiState
+
+    private val _isProgressOn = MutableStateFlow(false)
+    val isProgressOn: StateFlow<Boolean> get() = _isProgressOn
+
     val userId = MutableStateFlow("")
     val password = MutableStateFlow("")
 
     fun signin() {
         viewModelScope.launch {
+            _uiState.value = UiState.Loading
             signinUseCase(userId.value, password.value)
                 .onSuccess {
-                    println("로그인 성공")
+                    _uiState.value = UiState.Success(it)
                 }.onFailure {
-                    println("로그인 실패")
+                    _uiState.value = UiState.Error(it)
                 }
         }
+    }
+
+    fun setProgress(isProgressBarOn: Boolean) {
+        _isProgressOn.value = isProgressBarOn
     }
 }
