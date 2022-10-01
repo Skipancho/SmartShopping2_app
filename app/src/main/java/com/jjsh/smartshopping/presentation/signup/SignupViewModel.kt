e39a0a769ba5e2315f5e9914776b754ca542f85e
skipancho <ctg1770a@gmail.com>
2022-10-01 16:02:53 +0900

    [FEATURE] #13 - 회원 가입 화면 UI 구성 (디자인 수정 필요)

---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/signup/SignupViewModel.kt
@@ -0,0 +1,10 @@
+package com.jjsh.smartshopping.presentation.signup
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignupViewModel @Inject constructor() : ViewModel() {
+
+}
