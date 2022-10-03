e8ffdd8afd9be115522172cfa27e461b57b04d75
skipancho <ctg1770a@gmail.com>
2022-10-04 00:11:38 +0900

    [REFACTOR] #13 - 중복 사용하는 flowWithLifecycle 부분 함수화

--- app/src/main/java/com/jjsh/smartshopping/presentation/signin/SigninActivity.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/signin/SigninActivity.kt
@@ -2,8 +2,6 @@ package com.jjsh.smartshopping.presentation.signin
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
 import com.jjsh.smartshopping.R
 import com.jjsh.smartshopping.databinding.ActivitySigninBinding
 import com.jjsh.smartshopping.presentation.ErrorHandler
@@ -12,8 +10,6 @@ import com.jjsh.smartshopping.presentation.UiState
 import com.jjsh.smartshopping.presentation.base.BaseActivity
 import com.jjsh.smartshopping.presentation.extension.clearTaskAndStart
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class SigninActivity : BaseActivity<ActivitySigninBinding>() {
@@ -32,24 +28,23 @@ class SigninActivity : BaseActivity<ActivitySigninBinding>() {
 
     private fun observeData() {
         val errorHandler = ErrorHandler(this)
-        viewModel.uiState.flowWithLifecycle(lifecycle)
-            .onEach {
-                when (it) {
-                    is UiState.Loading -> {
-                        viewModel.setProgress(true)
-                    }
-                    is UiState.Success -> {
-                        viewModel.setProgress(false)
-                        clearTaskAndStart<MainActivity>()
-                    }
-                    is UiState.Error -> {
-                        viewModel.setProgress(false)
-                        errorHandler.errorHandling(it.err)
-                    }
-                    else -> {
-                        //do nothing
-                    }
+        observeFlowWithLifecycle(viewModel.uiState){
+            when (it) {
+                is UiState.Loading -> {
+                    viewModel.setProgress(true)
                 }
-            }.launchIn(lifecycleScope)
+                is UiState.Success -> {
+                    viewModel.setProgress(false)
+                    clearTaskAndStart<MainActivity>()
+                }
+                is UiState.Error -> {
+                    viewModel.setProgress(false)
+                    errorHandler.errorHandling(it.err)
+                }
+                else -> {
+                    //do nothing
+                }
+            }
+        }
     }
 }
