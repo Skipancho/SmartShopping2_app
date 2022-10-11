7e58c5c283570fe2e2a0c0db89bb6e9b28f1d557
skipancho <ctg1770a@gmail.com>
2022-10-11 21:36:40 +0900

    [FEATURE] #18 - MainActivity 에서 사용할Fragment 생성

---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/main/chart/ChartFragment.kt
@@ -0,0 +1,9 @@
+package com.jjsh.smartshopping.presentation.main.chart
+
+import com.jjsh.smartshopping.R
+import com.jjsh.smartshopping.databinding.FragmentChartBinding
+import com.jjsh.smartshopping.presentation.base.BaseFragment
+
+class ChartFragment : BaseFragment<FragmentChartBinding>(R.layout.fragment_chart) {
+
+}
---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/main/checklist/CheckListFragment.kt
@@ -0,0 +1,10 @@
+package com.jjsh.smartshopping.presentation.main.checklist
+
+import com.jjsh.smartshopping.R
+import com.jjsh.smartshopping.databinding.FragmentCheckListBinding
+import com.jjsh.smartshopping.presentation.base.BaseFragment
+
+
+class CheckListFragment : BaseFragment<FragmentCheckListBinding>(R.layout.fragment_check_list){
+
+}
