76e1f4f30d6b7fa19f7ee354e168de0ab50c02f5
skipancho <ctg1770a@gmail.com>
2022-10-16 19:41:56 +0900

    [FEATURE] #19 - mypage 기본 UI + back 버튼 추가

--- app/src/main/java/com/jjsh/smartshopping/presentation/mypage/MypageActivity.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/mypage/MypageActivity.kt
@@ -1,10 +1,38 @@
 package com.jjsh.smartshopping.presentation.mypage
 
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
 import com.jjsh.smartshopping.R
 import com.jjsh.smartshopping.databinding.ActivityMypageBinding
 import com.jjsh.smartshopping.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MypageActivity : BaseActivity<ActivityMypageBinding>(R.layout.activity_mypage) {
+
+    private val viewModel by viewModels<MypageViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initActionBar()
+    }
+
+    private fun initActionBar(){
+        setSupportActionBar(binding.mtbToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_24)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun finish() {
         super.finish()
---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/mypage/MypageViewModel.kt
@@ -0,0 +1,12 @@
+package com.jjsh.smartshopping.presentation.mypage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MypageViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
