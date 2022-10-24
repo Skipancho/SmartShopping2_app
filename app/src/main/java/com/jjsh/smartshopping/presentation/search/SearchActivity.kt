5bc9d8d35a773457e58d111e65f04ee887ccdc25
skipancho <ctg1770a@gmail.com>
2022-10-24 20:56:38 +0900

    [FEATURE] #31 - 검색창 포커스에 따라 fragment replace 기능 추가, 관련 fragment 생성

--- app/src/main/java/com/jjsh/smartshopping/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/search/SearchActivity.kt
@@ -1,14 +1,45 @@
 package com.jjsh.smartshopping.presentation.search
 
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
+import androidx.fragment.app.commit
 import com.jjsh.smartshopping.R
 import com.jjsh.smartshopping.databinding.ActivitySearchBinding
 import com.jjsh.smartshopping.presentation.base.BaseActivity
+import com.jjsh.smartshopping.presentation.search.history.SearchHistoryFragment
+import com.jjsh.smartshopping.presentation.search.result.SearchResultFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchActivity : BaseActivity<ActivitySearchBinding>(R.layout.activity_search) {
+
+    private val viewModel by viewModels<SearchViewModel>()
+    private val historyFragment by lazy { SearchHistoryFragment() }
+    private val resultFragment by lazy { SearchResultFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding.viewModel = viewModel
+        viewModel.setMoveToBack { finish() }
+        observeData()
+    }
+
+    private fun observeData() {
+        val inputMethodManager = this.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+        observeFlowWithLifecycle(viewModel.hasSearchViewFocus) {
+            if (it) {
+                supportFragmentManager.commit {
+                    replace(binding.flFragment.id, historyFragment)
+                }
+            } else {
+                supportFragmentManager.commit {
+                    replace(binding.flFragment.id, resultFragment)
+                }
+                inputMethodManager.hideSoftInputFromWindow(binding.stvSearch.windowToken, 0)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/search/result/SearchResultFragment.kt
@@ -0,0 +1,9 @@
+package com.jjsh.smartshopping.presentation.search.result
+
+import com.jjsh.smartshopping.R
+import com.jjsh.smartshopping.databinding.FragmentSearchResultBinding
+import com.jjsh.smartshopping.presentation.base.BaseFragment
+
+class SearchResultFragment : BaseFragment<FragmentSearchResultBinding>(R.layout.fragment_search_result) {
+
+}
