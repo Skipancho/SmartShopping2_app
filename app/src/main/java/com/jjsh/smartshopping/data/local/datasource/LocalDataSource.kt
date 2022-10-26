78df15213c7665d200aa9bbabacb7fea3a686381
skipancho <ctg1770a@gmail.com>
2022-10-26 20:42:06 +0900

    [FEATURE] #34 - LocalDataSource 구현

---
+++ app/src/main/java/com/jjsh/smartshopping/data/local/datasource/LocalDataSource.kt
@@ -0,0 +1,11 @@
+package com.jjsh.smartshopping.data.local.datasource
+
+import com.jjsh.smartshopping.data.local.dto.SearchHistoryDto
+import kotlinx.coroutines.flow.Flow
+
+
+interface LocalDataSource {
+    fun getSearchHistory(): Flow<Result<List<SearchHistoryDto>>>
+    suspend fun insertSearchHistory(historyDto: SearchHistoryDto): Result<Unit>
+    suspend fun deleteSearchHistory(historyDto: SearchHistoryDto): Result<Unit>
+}
