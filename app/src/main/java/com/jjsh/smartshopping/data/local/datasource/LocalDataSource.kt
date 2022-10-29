5a2aecc5d66a60bd926df8cdc2fc1fc0b483d1cf
skipancho <ctg1770a@gmail.com>
2022-10-29 20:07:45 +0900

    [FEATURE] #34 - LocalDataSource, 장바구니 목록 (CartItem) 관련 함수 추가

--- app/src/main/java/com/jjsh/smartshopping/data/local/datasource/LocalDataSource.kt
+++ app/src/main/java/com/jjsh/smartshopping/data/local/datasource/LocalDataSource.kt
@@ -1,5 +1,6 @@
 package com.jjsh.smartshopping.data.local.datasource
 
+import com.jjsh.smartshopping.data.local.dto.CartItemDto
 import com.jjsh.smartshopping.data.local.dto.SearchHistoryDto
 import kotlinx.coroutines.flow.Flow
 
@@ -8,4 +9,9 @@ interface LocalDataSource {
     fun getSearchHistory(): Flow<Result<List<SearchHistoryDto>>>
     suspend fun insertSearchHistory(historyDto: SearchHistoryDto): Result<Unit>
     suspend fun deleteSearchHistory(vararg historyDto: SearchHistoryDto): Result<Unit>
+
+    fun getCartItems(userCode: Long): Flow<Result<List<CartItemDto>>>
+    suspend fun getCartItem(userCode: Long, itemId: Long): Result<CartItemDto>
+    suspend fun insertCartItem(vararg cartItemDto: CartItemDto): Result<Unit>
+    suspend fun deleteCartItem(vararg cartItemDto: CartItemDto): Result<Unit>
 }
