52c107970de92dddef69edffd2428bc87aa9c432
skipancho <ctg1770a@gmail.com>
2022-10-29 19:53:42 +0900

    [FEATURE] #34 - 장바구니 목록 (CartItem) 관련 Dto, Dao 구현

--- app/src/main/java/com/jjsh/smartshopping/data/local/SmartShoppingDB.kt
+++ app/src/main/java/com/jjsh/smartshopping/data/local/SmartShoppingDB.kt
@@ -2,10 +2,13 @@ package com.jjsh.smartshopping.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.jjsh.smartshopping.data.local.dao.CartItemDao
 import com.jjsh.smartshopping.data.local.dao.SearchHistoryDao
+import com.jjsh.smartshopping.data.local.dto.CartItemDto
 import com.jjsh.smartshopping.data.local.dto.SearchHistoryDto
 
-@Database(entities = [SearchHistoryDto::class], version = 1)
+@Database(entities = [SearchHistoryDto::class, CartItemDto::class], version = 1)
 abstract class SmartShoppingDB : RoomDatabase() {
     abstract fun searchHistoryDao(): SearchHistoryDao
+    abstract fun cartItemDao(): CartItemDao
 }
