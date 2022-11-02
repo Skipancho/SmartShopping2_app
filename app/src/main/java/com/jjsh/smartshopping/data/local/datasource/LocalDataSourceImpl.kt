a66623abc145f9fca536ea2a1eb2cb25a3f1aa7b
skipancho <ctg1770a@gmail.com>
2022-11-02 19:54:42 +0900

    [FEATURE] #34 - LocalDataSource, 체크리스트 목록 (CheckItem) 관련 함수 추가

--- app/src/main/java/com/jjsh/smartshopping/data/local/datasource/LocalDataSourceImpl.kt
+++ app/src/main/java/com/jjsh/smartshopping/data/local/datasource/LocalDataSourceImpl.kt
@@ -1,8 +1,10 @@
 package com.jjsh.smartshopping.data.local.datasource
 
 import com.jjsh.smartshopping.data.local.dao.CartItemDao
+import com.jjsh.smartshopping.data.local.dao.CheckItemDao
 import com.jjsh.smartshopping.data.local.dao.SearchHistoryDao
 import com.jjsh.smartshopping.data.local.dto.CartItemDto
+import com.jjsh.smartshopping.data.local.dto.CheckItemDto
 import com.jjsh.smartshopping.data.local.dto.SearchHistoryDto
 import com.jjsh.smartshopping.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
@@ -16,6 +18,7 @@ import javax.inject.Inject
 class LocalDataSourceImpl @Inject constructor(
     private val searchHistoryDao: SearchHistoryDao,
     private val cartItemDao: CartItemDao,
+    private val checkItemDao: CheckItemDao,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : LocalDataSource {
 
@@ -70,6 +73,34 @@ class LocalDataSourceImpl @Inject constructor(
     override suspend fun deleteCartItem(vararg cartItemDto: CartItemDto): Result<Unit> {
         return withContext(ioDispatcher) {
             runCatching { cartItemDao.deleteItem(*cartItemDto) }
+        }
+    }
+
+    override fun getCheckItems(userCode: Long): Flow<Result<List<CheckItemDto>>> {
+        return checkItemDao
+            .getItems(userCode)
+            .map {
+                Result.success(it)
+            }.catch {
+                emit(Result.failure(Exception()))
+            }.flowOn(ioDispatcher)
+    }
+
+    override suspend fun getCheckItem(userCode: Long, itemId: Long): Result<CheckItemDto> {
+        return withContext(ioDispatcher) {
+            runCatching { checkItemDao.getItem(userCode, itemId) }
+        }
+    }
+
+    override suspend fun insertCheckItem(vararg checkItemDto: CheckItemDto): Result<Unit> {
+        return withContext(ioDispatcher){
+            runCatching { checkItemDao.insertItem(*checkItemDto) }
+        }
+    }
+
+    override suspend fun deleteCheckItem(vararg checkItemDto: CheckItemDto): Result<Unit> {
+        return withContext(ioDispatcher){
+            runCatching { checkItemDao.deleteItem(*checkItemDto) }
         }
     }
 }
