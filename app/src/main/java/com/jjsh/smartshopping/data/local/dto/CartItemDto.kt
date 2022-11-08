bd0606ffce555b224884e59a2edb172dc74b105e
skipancho <ctg1770a@gmail.com>
2022-11-08 19:28:05 +0900

    [BUGFIX] #47 - 장바구니 목록 등록 로직 수정

--- app/src/main/java/com/jjsh/smartshopping/data/local/dto/CartItemDto.kt
+++ app/src/main/java/com/jjsh/smartshopping/data/local/dto/CartItemDto.kt
@@ -7,7 +7,7 @@ import java.util.*
 
 @Entity(tableName = "CART_ITEM")
 data class CartItemDto(
-    @PrimaryKey(autoGenerate = true) val id: Long,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val productId: Long,
     val userCode: Long,
     val name: String,
@@ -18,8 +18,12 @@ data class CartItemDto(
     val isChecked: Boolean,
     val time: Long
 ) {
-    fun toCartItem(): CartItem = CartItem(
+    fun toCartItem() = CartItem(
         id, productId, name, nPrice, sPrice, amount, thumbnailUrl, isChecked
+    )
+
+    fun setId(id: Long) = CartItemDto(
+        id, productId, userCode, name, nPrice, sPrice, amount, thumbnailUrl, isChecked, time
     )
 }
 
