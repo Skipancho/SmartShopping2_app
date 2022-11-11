441d2136a674319d41c13422b874a51ac49e6e3e
skipancho <ctg1770a@gmail.com>
2022-11-11 19:03:51 +0900

    [FEATURE] #49 - 체크리스트 목록 업데이트 로직 수정 및 삭제 기능 추가

--- app/src/main/java/com/jjsh/smartshopping/presentation/adapter/CheckListAdapter.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/adapter/CheckListAdapter.kt
@@ -9,25 +9,27 @@ import com.jjsh.smartshopping.databinding.ItemChecklistBinding
 import com.jjsh.smartshopping.domain.model.CheckItem
 
 class CheckListAdapter(
-    private val _updateCheckItem: (CheckItem) -> Unit = {}
+    private val _updateCheckItem: (CheckItem) -> Unit = {},
+    private val deleteCheckItem: (CheckItem) -> Unit = {}
 ) : ListAdapter<CheckItem, CheckListAdapter.CheckItemViewHolder>(
     ItemDiffCallBack<CheckItem>({ old, new -> old.id == new.id })
 ) {
     private val updateCheckItem: (CheckItem, Int) -> Unit
         get() = { item, position ->
             _updateCheckItem(item)
-            notifyItemChanged(position, item.amount)
+            notifyItemChanged(position, item)
         }
 
     class CheckItemViewHolder(
         private val binding: ItemChecklistBinding,
-        private val updateCheckItem: (CheckItem, Int) -> Unit
+        private val updateCheckItem: (CheckItem, Int) -> Unit,
+        private val deleteCheckItem: (CheckItem) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: CheckItem) {
             binding.checkItem = item
             binding.btnMinus.setOnClickListener {
-                if (item.amount > 0) {
+                if (item.amount > 1) {
                     item.amount--
                     updateCheckItem(item, adapterPosition)
                 }
@@ -38,23 +40,31 @@ class CheckListAdapter(
                     updateCheckItem(item, adapterPosition)
                 }
             }
+            binding.ibDelete.setOnClickListener {
+                deleteCheckItem(item)
+            }
+            binding.cbIsChecked.setOnCheckedChangeListener { compoundButton, b ->
+                if (item.isChecked != b) item.isChecked = b
+                updateCheckItem(item, adapterPosition)
+            }
         }
 
         companion object {
             fun create(
                 parent: ViewGroup,
-                updateCheckItem: (CheckItem, Int) -> Unit
+                updateCheckItem: (CheckItem, Int) -> Unit,
+                deleteCheckItem: (CheckItem) -> Unit
             ): CheckItemViewHolder {
                 val view = LayoutInflater.from(parent.context)
                     .inflate(R.layout.item_checklist, parent, false)
                 val binding = ItemChecklistBinding.bind(view)
-                return CheckItemViewHolder(binding, updateCheckItem)
+                return CheckItemViewHolder(binding, updateCheckItem, deleteCheckItem)
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CheckItemViewHolder {
-        return CheckItemViewHolder.create(parent, updateCheckItem)
+        return CheckItemViewHolder.create(parent, updateCheckItem, deleteCheckItem)
     }
 
     override fun onBindViewHolder(holder: CheckItemViewHolder, position: Int) {
