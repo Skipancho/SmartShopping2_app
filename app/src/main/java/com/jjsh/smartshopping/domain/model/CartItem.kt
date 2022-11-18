6791025cc2913684710eeb8220d905d6d4960174
skipancho <ctg1770a@gmail.com>
2022-11-18 22:19:06 +0900

    [FEATURE] #52 - 장바구니 목록 호출 UseCase 추가

--- app/src/main/java/com/jjsh/smartshopping/domain/model/CartItem.kt
+++ app/src/main/java/com/jjsh/smartshopping/domain/model/CartItem.kt
@@ -8,15 +8,20 @@ data class CartItem(
     val sPrice: Int,
     var amount: Int,
     val thumbnailUrl: String,
-    val isChecked: Boolean = true
+    val isChecked: Boolean = false,
+    val inCheckList: Boolean = true
 ) {
     val sale get() = (100 * (nPrice - sPrice)) / nPrice
+    val totalPrice get() = amount * sPrice
 
     fun setAmount(amount: Int): CartItem =
         CartItem(id, productId, name, nPrice, sPrice, amount, thumbnailUrl, isChecked)
 
     fun setChecked(isChecked: Boolean): CartItem =
         CartItem(id, productId, name, nPrice, sPrice, amount, thumbnailUrl, isChecked)
+
+    fun setInCheckList(inCheckList: Boolean): CartItem =
+        CartItem(id, productId, name, nPrice, sPrice, amount, thumbnailUrl, isChecked, inCheckList)
 }
 
 fun Product.toCartItem(amount: Int): CartItem =
