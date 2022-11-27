9c8aae904084ece6f96384540867090477e76313
skipancho <ctg1770a@gmail.com>
2022-11-27 21:16:32 +0900

    [FEATURE] #57 - PurchaseService 구현

---
+++ app/src/main/java/com/jjsh/smartshopping/data/remote/api/PurchaseService.kt
@@ -0,0 +1,22 @@
+package com.jjsh.smartshopping.data.remote.api
+
+import com.jjsh.smartshopping.data.remote.ApiResponse
+import com.jjsh.smartshopping.data.remote.request.PurchaseRequest
+import com.jjsh.smartshopping.data.remote.response.PurchaseResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface PurchaseService {
+    @POST("/api/v1/purchase")
+    suspend fun registerPurchaseRecord(
+        @Body purchaseRequest: PurchaseRequest
+    ): ApiResponse<Unit>
+
+    @GET("/api/v1/purchase")
+    suspend fun getPurchaseRecord(
+        @Query("year") year: Int,
+        @Query("month") month: Int
+    ): ApiResponse<List<PurchaseResponse>>
+}
