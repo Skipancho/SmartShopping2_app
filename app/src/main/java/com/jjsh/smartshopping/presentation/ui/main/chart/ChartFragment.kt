f0048315bad1df9dd439a4df0f520ce0bd4fe227
skipancho <ctg1770a@gmail.com>
2022-11-28 21:29:03 +0900

    [FEATURE] #60 - ChartFragment UI 구성

--- app/src/main/java/com/jjsh/smartshopping/presentation/ui/main/chart/ChartFragment.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/ui/main/chart/ChartFragment.kt
@@ -1,9 +1,56 @@
 package com.jjsh.smartshopping.presentation.ui.main.chart
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jjsh.smartshopping.R
 import com.jjsh.smartshopping.databinding.FragmentChartBinding
+import com.jjsh.smartshopping.presentation.UiState
+import com.jjsh.smartshopping.presentation.adapter.ChartAdapter
 import com.jjsh.smartshopping.presentation.base.BaseFragment
+import com.jjsh.smartshopping.presentation.extension.errorHandling
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ChartFragment : BaseFragment<FragmentChartBinding>(R.layout.fragment_chart) {
 
+    private val viewModel by viewModels<ChartViewModel>()
+    private val chartAdapter by lazy { ChartAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = viewModel
+
+        initView()
+        observeData()
+    }
+
+    private fun initView() {
+        with(binding.rvChart) {
+            adapter = chartAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            itemAnimator = null
+        }
+    }
+
+    private fun observeData() {
+        observeFlowWithLifecycle(viewModel.chartData) {
+            when(it) {
+                is UiState.Success -> {
+                    chartAdapter.submitList(it.data.toMutableList())
+                }
+                is UiState.Error -> {
+                    requireContext().errorHandling(it.err)
+                }
+                else -> {}
+            }
+        }
+    }
+
+    override fun onResume() {
+        viewModel.getChartData()
+        super.onResume()
+    }
 }
