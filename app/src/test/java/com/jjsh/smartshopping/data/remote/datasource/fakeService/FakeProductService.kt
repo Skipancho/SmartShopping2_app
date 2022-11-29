b9ecdf4ee0828746dae75765600544893b87ad5f
skipancho <ctg1770a@gmail.com>
2022-11-29 20:07:06 +0900

    [FEATURE] #64 - FakeService 추가 및 수정

--- app/src/test/java/com/jjsh/smartshopping/data/remote/datasource/fakeService/FakeProductService.kt
+++ app/src/test/java/com/jjsh/smartshopping/data/remote/datasource/fakeService/FakeProductService.kt
@@ -3,6 +3,7 @@ package com.jjsh.smartshopping.data.remote.datasource.fakeService
 import com.jjsh.smartshopping.data.remote.ApiResponse
 import com.jjsh.smartshopping.data.remote.api.ProductService
 import com.jjsh.smartshopping.data.remote.response.ProductResponse
+import retrofit2.http.Query
 
 class FakeProductService(
     private val fakeProductResponses: List<ProductResponse>
@@ -11,12 +12,17 @@ class FakeProductService(
         productId: Long,
         categoryId: Int?,
         direction: String,
-        keyword: String?
+        keyword: String?,
+        pageSize: Int
     ): ApiResponse<List<ProductResponse>> {
         return ApiResponse(true, fakeProductResponses, null)
     }
 
     override suspend fun getProduct(id: Long): ApiResponse<ProductResponse> {
         return ApiResponse(true, fakeProductResponses.find { it.id == id }, null)
+    }
+
+    override suspend fun findProductByBarcode(barcode: Long): ApiResponse<ProductResponse> {
+        return ApiResponse(true, fakeProductResponses[barcode.toInt()], null)
     }
 }
---
+++ app/src/test/java/com/jjsh/smartshopping/data/remote/datasource/fakeService/FakePurchaseService.kt
@@ -0,0 +1,21 @@
+package com.jjsh.smartshopping.data.remote.datasource.fakeService
+
+import com.jjsh.smartshopping.data.remote.ApiResponse
+import com.jjsh.smartshopping.data.remote.api.PurchaseService
+import com.jjsh.smartshopping.data.remote.request.PurchaseRequest
+import com.jjsh.smartshopping.data.remote.response.PurchaseResponse
+
+class FakePurchaseService(
+    private val fakePurchaseResponse: List<PurchaseResponse>
+) : PurchaseService {
+    override suspend fun registerPurchaseRecord(purchaseRequest: List<PurchaseRequest>): ApiResponse<Unit> {
+        return ApiResponse(true, null, null)
+    }
+
+    override suspend fun getPurchaseRecord(
+        year: Int,
+        month: Int
+    ): ApiResponse<List<PurchaseResponse>> {
+        return ApiResponse(true,fakePurchaseResponse,null)
+    }
+}
