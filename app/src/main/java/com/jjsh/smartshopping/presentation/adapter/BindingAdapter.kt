23bc3c4671a0979c28c1503de0b26017d3e5857b
skipancho <ctg1770a@gmail.com>
2023-01-05 18:27:06 +0900

    [FEATURE] #71 - 사용자의 리뷰 item UI 구성

--- app/src/main/java/com/jjsh/smartshopping/presentation/adapter/BindingAdapter.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/adapter/BindingAdapter.kt
@@ -1,11 +1,18 @@
 package com.jjsh.smartshopping.presentation.adapter
 
+import android.graphics.Color
 import android.graphics.Paint
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.jjsh.smartshopping.R
+import com.jjsh.smartshopping.common.DateUtil
 import com.jjsh.smartshopping.common.ImageLoader
+import com.jjsh.smartshopping.presentation.decoration.CustomDividerDecoration
+import com.jjsh.smartshopping.presentation.extension.dpToPx
+import java.util.*
 
 @BindingAdapter("image")
 fun ImageView.setImage(imageUrl: String) {
@@ -18,4 +25,9 @@ fun ImageView.setImage(imageUrl: String) {
 @BindingAdapter("centerLine")
 fun TextView.setCenterLine(visible: Boolean) {
     paintFlags = if (visible) paintFlags.or(Paint.STRIKE_THRU_TEXT_FLAG) else 0
+}
+
+@BindingAdapter("dotFormatDate")
+fun TextView.setDotFormatDate(date: Date) {
+    text = DateUtil.dateToString(date)
 }
