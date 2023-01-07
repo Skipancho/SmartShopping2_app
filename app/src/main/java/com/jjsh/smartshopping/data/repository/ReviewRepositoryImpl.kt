c96fd8807df2c3a270ebf76edb25d6f1cfe8f785
skipancho <ctg1770a@gmail.com>
2023-01-07 15:15:01 +0900

    [FEATURE] #71 - 리뷰 수정 및 삭제 관련 함수 추가

--- app/src/main/java/com/jjsh/smartshopping/data/repository/ReviewRepositoryImpl.kt
+++ app/src/main/java/com/jjsh/smartshopping/data/repository/ReviewRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.jjsh.smartshopping.data.repository
 
 import com.jjsh.smartshopping.data.remote.datasource.RemoteDataSource
 import com.jjsh.smartshopping.data.remote.request.ReviewRequest
+import com.jjsh.smartshopping.data.remote.response.ReviewResponse
 import com.jjsh.smartshopping.domain.model.Review
 import com.jjsh.smartshopping.domain.repository.ReviewRepository
 import javax.inject.Inject
@@ -9,6 +10,13 @@ import javax.inject.Inject
 class ReviewRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource
 ) : ReviewRepository {
+
+    override suspend fun getReview(reviewId: Long): Result<Review> {
+        return remoteDataSource.getReview(reviewId).mapCatching {
+            responseToReview(it)
+        }
+    }
+
     override suspend fun writeReview(
         productId: Long,
         purchaseId: Long,
@@ -20,14 +28,33 @@ class ReviewRepositoryImpl @Inject constructor(
         )
     }
 
+    override suspend fun updateReview(
+        productId: Long,
+        purchaseId: Long,
+        score: Int,
+        reviewText: String
+    ): Result<Unit> {
+        return remoteDataSource.updateReview(
+            ReviewRequest(productId, purchaseId, score, reviewText)
+        )
+    }
+
+    override suspend fun deleteReview(reviewId: Long): Result<Unit> {
+        return remoteDataSource.deleteReview(reviewId)
+    }
+
     override suspend fun getReviews(productId: Long?): Result<List<Review>> {
         return remoteDataSource.getReviews(productId).mapCatching { list ->
             list.map {
-                val product = remoteDataSource.getProduct(it.productId).getOrNull()
-                val productName = product?.name ?: "none"
-                val thumbnailUrl = product?.imagePaths?.firstOrNull() ?: ""
-                it.toReview(productName, thumbnailUrl)
+                responseToReview(it)
             }
         }
+    }
+
+    private suspend fun responseToReview(reviewResponse: ReviewResponse): Review {
+        val product = remoteDataSource.getProduct(reviewResponse.productId).getOrNull()
+        val productName = product?.name ?: "none"
+        val thumbnailUrl = product?.imagePaths?.firstOrNull() ?: ""
+        return reviewResponse.toReview(productName, thumbnailUrl)
     }
 }
--- app/src/main/java/com/jjsh/smartshopping/domain/repository/ReviewRepository.kt
+++ app/src/main/java/com/jjsh/smartshopping/domain/repository/ReviewRepository.kt
@@ -3,11 +3,27 @@ package com.jjsh.smartshopping.domain.repository
 import com.jjsh.smartshopping.domain.model.Review
 
 interface ReviewRepository {
+
+    suspend fun getReview(
+        reviewId: Long
+    ): Result<Review>
+
     suspend fun writeReview(
         productId: Long,
         purchaseId: Long,
         score: Int,
         reviewText: String
+    ): Result<Unit>
+
+    suspend fun updateReview(
+        productId: Long,
+        purchaseId: Long,
+        score: Int,
+        reviewText: String
+    ): Result<Unit>
+
+    suspend fun deleteReview(
+        reviewId: Long
     ): Result<Unit>
 
     suspend fun getReviews(
