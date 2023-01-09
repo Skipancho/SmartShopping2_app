54df225b24723b677cfba25df300d088761f5e6e
skipancho <ctg1770a@gmail.com>
2023-01-09 19:41:55 +0900

    [FEATURE] #79 - 회원 탈퇴 화면 UI 구성

---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/ui/mypage/withdrawal/WithdrawalActivity.kt
@@ -0,0 +1,13 @@
+package com.jjsh.smartshopping.presentation.ui.mypage.withdrawal
+
+import android.os.Bundle
+import com.jjsh.smartshopping.R
+import com.jjsh.smartshopping.databinding.ActivityWithdrawalBinding
+import com.jjsh.smartshopping.presentation.base.BaseActivity
+
+class WithdrawalActivity : BaseActivity<ActivityWithdrawalBinding>(R.layout.activity_withdrawal) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
