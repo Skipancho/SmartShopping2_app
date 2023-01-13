8e2cdc1187d2c43b6afd8b38603a5d4ed983de0e
skipancho <ctg1770a@gmail.com>
2023-01-13 21:05:34 +0900

    [FEATURE] - 상품 등록 화면 UI 구성 (1차)

---
+++ app/src/main/java/com/jjsh/smartshopping/presentation/ui/registration/product/ProductRegistrationActivity.kt
@@ -0,0 +1,14 @@
+package com.jjsh.smartshopping.presentation.ui.registration.product
+
+import android.os.Bundle
+import com.jjsh.smartshopping.R
+import com.jjsh.smartshopping.databinding.ActivityProductRegistrationBinding
+import com.jjsh.smartshopping.presentation.base.BaseActivity
+
+class ProductRegistrationActivity :
+    BaseActivity<ActivityProductRegistrationBinding>(R.layout.activity_product_registration) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
