495b6d8ba7a2a400d32eda3eb5387ef97423b0c0
skipancho <ctg1770a@gmail.com>
2023-01-13 19:56:17 +0900

    [FEATURE] #64 - RemoteDataSource 인터페이스 수정에 따른 함수 추가

--- app/src/test/java/com/jjsh/smartshopping/data/fake/datasource/FakeRemoteDataSource.kt
+++ app/src/test/java/com/jjsh/smartshopping/data/fake/datasource/FakeRemoteDataSource.kt
@@ -1,12 +1,16 @@
 package com.jjsh.smartshopping.data.fake.datasource
 
+import com.jjsh.smartshopping.common.ErrorException
 import com.jjsh.smartshopping.data.remote.datasource.RemoteDataSource
 import com.jjsh.smartshopping.data.remote.request.PurchaseRequest
+import com.jjsh.smartshopping.data.remote.request.ReviewRequest
 import com.jjsh.smartshopping.data.remote.request.SigninRequest
 import com.jjsh.smartshopping.data.remote.request.SignupRequest
 import com.jjsh.smartshopping.data.remote.response.ProductResponse
 import com.jjsh.smartshopping.data.remote.response.PurchaseResponse
+import com.jjsh.smartshopping.data.remote.response.ReviewResponse
 import com.jjsh.smartshopping.data.remote.response.SigninResponse
+import java.util.*
 
 class FakeRemoteDataSource : RemoteDataSource {
     override suspend fun signin(signinRequest: SigninRequest): Result<SigninResponse> {
@@ -30,6 +34,10 @@ class FakeRemoteDataSource : RemoteDataSource {
     }
 
     override suspend fun validateNickName(nickName: String): Result<Unit> {
+        return Result.success(Unit)
+    }
+
+    override suspend fun withdrawal(): Result<Unit> {
         return Result.success(Unit)
     }
 
@@ -75,12 +83,57 @@ class FakeRemoteDataSource : RemoteDataSource {
         return Result.success(
             listOf(
                 PurchaseResponse(
-                    1L,"category1",1L,"name1",10000,1
+                    1L,"category1",1L,"name1",10000,1,false
                 ),
                 PurchaseResponse(
-                    2L,"category2",2L,"name2",9000,10
+                    2L,"category2",2L,"name2",9000,10, false
                 )
             )
         )
+    }
+
+    private var _testDate : Date? = null
+    private val testDate : Date get() = _testDate ?: error("testDate is null")
+
+    fun setTestDate(date: Date) {
+        _testDate = date
+    }
+
+    override suspend fun getReview(reviewId: Long): Result<ReviewResponse> {
+        return runCatching {
+            listOf(
+                ReviewResponse(
+                    1L,1L,"nickname1",1L,1,"reviewText1", testDate
+                ),
+                ReviewResponse(
+                    2L,2L,"nickname2",2L,2,"reviewText2", testDate
+                )
+            ).find { it.id == reviewId } ?: throw ErrorException.ReviewException
+        }
+    }
+
+    override suspend fun writeReview(reviewRequest: ReviewRequest): Result<Unit> {
+        return Result.success(Unit)
+    }
+
+    override suspend fun updateReview(reviewRequest: ReviewRequest): Result<Unit> {
+        return Result.success(Unit)
+    }
+
+    override suspend fun deleteReview(reviewId: Long): Result<Unit> {
+        return Result.success(Unit)
+    }
+
+    override suspend fun getReviews(productId: Long?): Result<List<ReviewResponse>> {
+        return runCatching {
+            listOf(
+                ReviewResponse(
+                    1L,1L,"nickname1",1L,1,"reviewText1", testDate
+                ),
+                ReviewResponse(
+                    2L,2L,"nickname2",2L,2,"reviewText2", testDate
+                )
+            )
+        }
     }
 }
