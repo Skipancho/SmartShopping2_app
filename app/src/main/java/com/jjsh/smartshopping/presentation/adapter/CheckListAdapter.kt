ded668bfcbbc7013bb26d8cf872c3292b3e9e2c1
skipancho <ctg1770a@gmail.com>
2023-01-18 19:40:10 +0900

    [REFACTOR] - BaseAdapter 적용

--- app/src/main/java/com/jjsh/smartshopping/presentation/adapter/CheckListAdapter.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/adapter/CheckListAdapter.kt
@@ -1,18 +1,16 @@
 package com.jjsh.smartshopping.presentation.adapter
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.jjsh.smartshopping.R
 import com.jjsh.smartshopping.databinding.ItemChecklistBinding
 import com.jjsh.smartshopping.domain.model.CheckItem
+import com.jjsh.smartshopping.presentation.base.BaseAdapter
 
 class CheckListAdapter(
     private val _updateCheckItem: (CheckItem) -> Unit = {},
     private val deleteCheckItem: (CheckItem) -> Unit = {}
-) : ListAdapter<CheckItem, CheckListAdapter.CheckItemViewHolder>(
-    ItemDiffCallBack<CheckItem>({ old, new -> old.id == new.id })
+) : BaseAdapter<ItemChecklistBinding, CheckItem> (
+    checkItemsTheSame = { old, new -> old.id == new.id },
+    R.layout.item_checklist
 ) {
     private val updateCheckItem: (CheckItem, Int) -> Unit
         get() = { item, position ->
@@ -20,24 +18,19 @@ class CheckListAdapter(
             notifyItemChanged(position, item)
         }
 
-    class CheckItemViewHolder(
-        private val binding: ItemChecklistBinding,
-        private val updateCheckItem: (CheckItem, Int) -> Unit,
-        private val deleteCheckItem: (CheckItem) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(item: CheckItem) {
+    override val bind: (ItemChecklistBinding, CheckItem, Int) -> Unit
+        get() = { binding, item, position ->
             binding.checkItem = item
             binding.btnMinus.setOnClickListener {
                 if (item.amount > 1) {
                     item.amount--
-                    updateCheckItem(item, adapterPosition)
+                    updateCheckItem(item, position)
                 }
             }
             binding.btnPlus.setOnClickListener {
                 if (item.amount < 100) {
                     item.amount++
-                    updateCheckItem(item, adapterPosition)
+                    updateCheckItem(item, position)
                 }
             }
             binding.ibDelete.setOnClickListener {
@@ -45,29 +38,7 @@ class CheckListAdapter(
             }
             binding.cbIsChecked.setOnClickListener {
                 item.isChecked = !item.isChecked
-                updateCheckItem(item, adapterPosition)
+                updateCheckItem(item, position)
             }
         }
-
-        companion object {
-            fun create(
-                parent: ViewGroup,
-                updateCheckItem: (CheckItem, Int) -> Unit,
-                deleteCheckItem: (CheckItem) -> Unit
-            ): CheckItemViewHolder {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_checklist, parent, false)
-                val binding = ItemChecklistBinding.bind(view)
-                return CheckItemViewHolder(binding, updateCheckItem, deleteCheckItem)
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CheckItemViewHolder {
-        return CheckItemViewHolder.create(parent, updateCheckItem, deleteCheckItem)
-    }
-
-    override fun onBindViewHolder(holder: CheckItemViewHolder, position: Int) {
-        holder.bind(getItem(position))
-    }
 }
--- app/src/main/java/com/jjsh/smartshopping/presentation/adapter/PurchaseRecordAdapter.kt
+++ app/src/main/java/com/jjsh/smartshopping/presentation/adapter/PurchaseRecordAdapter.kt
@@ -1,45 +1,21 @@
 package com.jjsh.smartshopping.presentation.adapter
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.jjsh.smartshopping.R
 import com.jjsh.smartshopping.databinding.ItemPurchaseRecordBinding
 import com.jjsh.smartshopping.domain.model.Purchase
+import com.jjsh.smartshopping.presentation.base.BaseAdapter
 
 class PurchaseRecordAdapter(
     private val onClick: (Purchase) -> Unit = {}
-) : ListAdapter<Purchase, PurchaseRecordAdapter.PurchaseRecordViewHolder>(
-    ItemDiffCallBack<Purchase>({ old, new -> old.id == new.id })
+) : BaseAdapter<ItemPurchaseRecordBinding, Purchase>(
+    checkItemsTheSame = { old, new -> old.id == new.id },
+    R.layout.item_purchase_record
 ) {
-    class PurchaseRecordViewHolder(
-        private val binding: ItemPurchaseRecordBinding,
-        private val onClick: (Purchase) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(item: Purchase) {
+    override val bind: (ItemPurchaseRecordBinding, Purchase, Int) -> Unit
+        get() = { binding, item, _ ->
             binding.purchase = item
             binding.root.setOnClickListener {
                 onClick(item)
             }
         }
-
-        companion object {
-            fun create(parent: ViewGroup, onClick: (Purchase) -> Unit): PurchaseRecordViewHolder {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_purchase_record, parent, false)
-                val binding = ItemPurchaseRecordBinding.bind(view)
-                return PurchaseRecordViewHolder(binding, onClick)
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PurchaseRecordViewHolder {
-        return PurchaseRecordViewHolder.create(parent, onClick)
-    }
-
-    override fun onBindViewHolder(holder: PurchaseRecordViewHolder, position: Int) {
-        holder.bind(getItem(position))
-    }
 }
